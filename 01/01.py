34856213a784efbb8a2a2adfaedcee7ffec60ee7
axel-nils <axelnils@student.chalmers.se>
2024-12-02 00:10:23 +0100

    added template and formatting

--- 01/01.py
+++ 01/01.py
@@ -1,9 +1,11 @@
 import numpy as np
 import pandas as pd
 
+
 def read():
     df = pd.read_csv("01/01.txt", sep="\\s+", header=None)
     return df[[0]], df[[1]]
+
 
 def main():
     # Part 1
@@ -15,22 +17,21 @@ def main():
     print(f"Total distance: {distances["Locations"].sum()}")
 
     # Part 2
-    occurances = right_list.groupby(["Locations"]).size() 
+    occurances = right_list.groupby(["Locations"]).size()
 
     def find_occurances(x):
         if x in occurances.keys():
             return occurances[x]
         else:
             return 0
-        
+
     similarity_sum = 0
     for i in range(left_list.size):
         l = left_list["Locations"].iloc[i]
         score = l * find_occurances(l)
         similarity_sum += score
-    
-    print(f"Total similarity: {similarity_sum}")
 
+    print(f"Total similarity: {similarity_sum}")
 
 
 if __name__ == "__main__":
---
+++ template/XX.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+
+
+def read():
+    df = pd.read_csv("XX/XX.txt", sep="\\s+", header=None)
+    return df
+
+
+def main():
+    # Part 1
+    pass
+
+    # Part 2
+    pass
+
+
+if __name__ == "__main__":
+    main()
